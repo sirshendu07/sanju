812b015c82c6bebae49d1397cdc444f156780442
sirshendu07 <sirshendubera386@gmail.com>
2024-09-08 14:37:09 +0000

    strings done

---
+++ test/strings.js
@@ -0,0 +1,16 @@
+const name="sirshendu"
+const passcode="00007"
+
+console.log(name +passcode + "sanju")
+
+console.log("hello my name is ${name} my passcode is ${passcode} ");  //doubt
+
+
+const game = new String("sanju")
+
+console.log(game[0]);
+console.log(game[3]);
+console.log(game.__proto__);
+
+console.log(game.length);
+console.log(game.toUpperCase());
